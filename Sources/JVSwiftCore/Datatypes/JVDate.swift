cd5c3a8e8d76972a579f5c87106da92a0cd4be1b
TheMisfit68 <janverrept@me.com>
2024-02-05 18:59:43 +0100

    add convinenience methods to Date to work with locals

--- Sources/JVSwiftCore/Datatypes/JVDate.swift
+++ Sources/JVSwiftCore/Datatypes/JVDate.swift
@@ -8,15 +8,23 @@
 import Foundation
 
 public extension Date {
-    
-    var utcToLocal: Date {
-        let secondsFromUTC = Double(Calendar.current.timeZone.secondsFromGMT(for: self))
-        return self+secondsFromUTC
-    }
-    
-    var noon: Date {
-        return Calendar.current.startOfDay(for: self)+(12*3600)
-    }
-    
-  
+	
+	var utcToLocal: Date {
+		let secondsFromUTC = Double(Calendar.current.timeZone.secondsFromGMT(for: self))
+		return self+secondsFromUTC
+	}
+	
+	var noon: Date {
+		return Calendar.current.startOfDay(for: self)+(12*3600)
+	}
+	
+	// MARK: - String representations
+	func localDateTimeString() -> String {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .short
+		formatter.timeStyle = .medium
+		formatter.locale = Locale.current // Adjusts the format to the user's current locale
+		return formatter.string(from: self)
+	}
+	
 }
