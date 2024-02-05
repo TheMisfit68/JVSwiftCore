09efbeb28a6117b86f43e262f31100be3b6cc70c
TheMisfit68 <janverrept@me.com>
2024-02-05 18:59:06 +0100

    Add set and reset to standard Bool

--- Sources/JVSwiftCore/Datatypes/JVBool.swift
+++ Sources/JVSwiftCore/Datatypes/JVBool.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
+// MARK: - SET/RESET
+public extension Bool {
+	
+	mutating func set(_ condition:Bool = true){
+		if condition{
+			self = true
+		}
+	}
+	
+	mutating func reset(_ condition:Bool = true){
+		if condition{
+			self = false
+		}
+	}
+	
+}
+
+public func set(_ boolean:inout Bool){
+	boolean.set()
+}
+
+public func reset(_ boolean:inout Bool){
+	boolean.reset()
+}
+
+
 /**
  Swift Logical XOR operator (a.k.a. 'Bit controlled inverter')
  
