08f9a2b169cebdfa1fc9d60656a69837df398a41
TheMisfit68 <janverrept@me.com>
2024-02-19 00:59:39 +0100

    Made extensions on Codable types public

--- Sources/JVSwiftCore/Datatypes/JVCodable.swift
+++ Sources/JVSwiftCore/Datatypes/JVCodable.swift
@@ -9,29 +9,29 @@ import Foundation
 
 // Make it possible to Create/Decode a Decodable object from a Data object
 // Such Data objects might e.g. be returned from a REST API request
-extension Decodable{
-	/// Initialize an object from Data
-	public init?(from data: Data, dateDecodingStrategy:JSONDecoder.DateDecodingStrategy = .deferredToDate) {
+public extension Decodable{
+
+	init?(from data: Data, dateDecodingStrategy:JSONDecoder.DateDecodingStrategy = .deferredToDate) {
 		let jsonDecoder = JSONDecoder()
 		jsonDecoder.dateDecodingStrategy = dateDecodingStrategy
-		guard let decodedObject = try? jsonDecoder.decode(Self.self, from: data) else {
-			return nil
-		}
+		guard let decodedObject = try? jsonDecoder.decode(Self.self, from: data) else {return nil}
 		self = decodedObject
 	}
+	
 }
 
 // Provide a debugDescription for all Codable objects
-extension CustomDebugStringConvertible where Self: Codable{
-	
+// (creating an extension for 'CustomStringConvertible where Self:Codable' seems incopatible with existing implementations in JVNetworkingt)
+public extension Encodable{
+
 	/// Return a pretty-printed JSON string
-	public var debugDescription:String {
+	var debugDescription:String {
 		let encoder = JSONEncoder()
 		encoder.outputFormatting = .prettyPrinted
-		guard let jsonData = try? encoder.encode(self),
-			  let prettyPrintedString = String(data: jsonData, encoding: .utf8) else {
+		guard let jsonData = try? encoder.encode(self), let prettyPrintedString = String(data: jsonData, encoding: .utf8) else {
 			return "Error: Unable to create pretty-printed JSON string."
 		}
 		return prettyPrintedString
 	}
+	
 }
